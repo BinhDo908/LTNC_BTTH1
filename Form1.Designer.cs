e7b242bd362c6044618c2c1494369086c241e0a9
BinhDo908 <ducbinhdo908@gmail.com>
2025-09-26 23:20:42 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace LTNC_BTTH2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rad1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rad2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnKQ = new System.Windows.Forms.Button();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rad1
+            // 
+            this.rad1.AutoSize = true;
+            this.rad1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad1.Location = new System.Drawing.Point(26, 58);
+            this.rad1.Name = "rad1";
+            this.rad1.Size = new System.Drawing.Size(106, 23);
+            this.rad1.TabIndex = 0;
+            this.rad1.TabStop = true;
+            this.rad1.Text = "chữ thường";
+            this.rad1.UseVisualStyleBackColor = true;
+            this.rad1.CheckedChanged += new System.EventHandler(this.rad1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập họ và tên:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoTen.Location = new System.Drawing.Point(300, 78);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(323, 34);
+            this.txtHoTen.TabIndex = 2;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.txtHoTen_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rad2);
+            this.panel1.Controls.Add(this.rad1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(142, 166);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 128);
+            this.panel1.TabIndex = 3;
+            // 
+            // rad2
+            // 
+            this.rad2.AutoSize = true;
+            this.rad2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad2.Location = new System.Drawing.Point(26, 87);
+            this.rad2.Name = "rad2";
+            this.rad2.Size = new System.Drawing.Size(105, 23);
+            this.rad2.TabIndex = 0;
+            this.rad2.TabStop = true;
+            this.rad2.Text = "CHỮ HOA";
+            this.rad2.UseVisualStyleBackColor = true;
+            this.rad2.CheckedChanged += new System.EventHandler(this.rad1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chọn kiểu chữ:";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.Window;
+            this.btnXoa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnXoa.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btnXoa.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
+            this.btnXoa.FlatAppearance.BorderSize = 10;
+            this.btnXoa.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnXoa.Location = new System.Drawing.Point(630, 166);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(84, 128);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnKQ
+            // 
+            this.btnKQ.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnKQ.FlatAppearance.BorderSize = 10;
+            this.btnKQ.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnKQ.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnKQ.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKQ.Location = new System.Drawing.Point(122, 315);
+            this.btnKQ.Name = "btnKQ";
+            this.btnKQ.Size = new System.Drawing.Size(118, 56);
+            this.btnKQ.TabIndex = 5;
+            this.btnKQ.Text = "Kết quả";
+            this.btnKQ.UseVisualStyleBackColor = true;
+            this.btnKQ.Click += new System.EventHandler(this.btnKQ_Click);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKQ.Location = new System.Drawing.Point(264, 326);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(359, 34);
+            this.txtKQ.TabIndex = 2;
+            this.txtKQ.TextChanged += new System.EventHandler(this.txtHoTen_TextChanged);
+            // 
+            // btnDung
+            // 
+            this.btnDung.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnDung.FlatAppearance.BorderSize = 10;
+            this.btnDung.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnDung.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDung.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDung.Location = new System.Drawing.Point(122, 377);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(118, 41);
+            this.btnDung.TabIndex = 5;
+            this.btnDung.Text = "Dừng";
+            this.btnDung.UseVisualStyleBackColor = true;
+            this.btnDung.Click += new System.EventHandler(this.btnDung_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnKQ);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Đổi kiểu chữ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rad1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rad2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnKQ;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnDung;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTNC_BTTH2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnKQ_Click(object sender, EventArgs e)
+        {
+            string hoten = this.txtHoTen.Text.Trim();
+            if (this.rad1.Checked == true)
+            {
+                txtKQ.Text = hoten.ToLower();
+            }
+            if (this.rad2.Checked == true)
+            {
+                txtKQ.Text = hoten.ToUpper();
+            }
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtHoTen.Clear();
+            this.txtKQ.Clear();
+            this.rad1.Checked = true;
+            this.txtHoTen.Focus();
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void txtHoTen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rad1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
